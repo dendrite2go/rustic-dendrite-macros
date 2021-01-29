00a77f9755a4038506e6f8ba975196d00f84b5e4
Jeroen van Maanen <jeroen@leialearns.org>
2021-01-29 07:23:28 +0100

    Initial commit: baby steps.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 extern crate proc_macro;
 
-use log::debug;
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{FnArg, Ident, ItemFn, Pat, PatIdent, PatType, Signature, Type, parse_macro_input};
@@ -16,16 +15,28 @@ pub fn event_handler(_attr: TokenStream, item: TokenStream) -> TokenStream {
 }
 
 fn impl_event_handler(ast: &ItemFn) -> TokenStream {
-    debug!("AST: {:?}", ast);
-    let ItemFn{sig: Signature {ident, inputs, ..}, ..} = ast;
-    debug!("X: {:?}", ident);
+    println!("AST: {:?}", ast);
+    let ItemFn{vis, sig, block, ..} = ast;
+    let Signature {ident, inputs, output, ..} = sig;
+    let ident_string = ident.to_string();
+    let ident_span = ident.span();
+    println!("X: {:?}: {:?}: {:?}", ident, ident_string, ident_span);
+    let ident_helper = Ident::new(&format!("{}_helper", ident_string), ident_span);
+    let ident_impl = Ident::new(&format!("{}_impl", ident_string), ident_span);
     let mut arg_iter = inputs.iter();
     let (event_arg_name, event_type) = split_argument(arg_iter.next().unwrap());
     let (query_model_arg_name, _query_model_type) = split_argument(arg_iter.next().unwrap());
     let gen = quote! {
-        #ast
-        fn test(#query_model_arg_name: #_query_model_type, #event_arg_name: #event_type) -> () {
-            #ident(#event_arg_name, #query_model_arg_name)
+        #vis async fn test(#inputs) #output #block
+
+        async fn #ident_impl(#query_model_arg_name: #_query_model_type, #event_arg_name: #event_type) -> () {
+            test(#event_arg_name, #query_model_arg_name).await
+        }
+
+        async fn #ident_helper<T: AsyncApplicableTo<P>,P: Clone>(event: Box<T>, projection: P) -> Result<()> {
+            let mut p = projection.clone();
+            event.apply_to(&mut p).await?;
+            Ok(())
         }
     };
     gen.into()
