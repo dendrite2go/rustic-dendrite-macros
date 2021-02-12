c171b3f956fd2fb3330ed304ff3bbde79c8f1c43
Jeroen van Maanen <jeroen@leialearns.org>
2021-02-12 13:28:11 +0100

    Added macro dendrite::command_handler.

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{FnArg, Ident, ItemFn, LitStr, Pat, PatIdent, Path, PatType, Signature, Type, TypePath, parse_macro_input};
+use syn::{AngleBracketedGenericArguments, FnArg, GenericArgument, Ident, ItemFn, LitStr, Pat, PatIdent, Path, PathArguments, PathSegment, PatType, ReturnType, Signature, Type, TypePath, TypeReference, parse_macro_input};
 
 #[proc_macro_attribute]
 pub fn event_handler(_attr: TokenStream, item: TokenStream) -> TokenStream {
@@ -25,10 +25,10 @@ fn impl_event_handler(ast: &ItemFn) -> TokenStream {
     let ident_helper = Ident::new(&format!("{}_helper", ident_string), ident_span);
 
     let mut arg_iter = inputs.iter();
-    let (event_arg_name, event_type) = split_argument(arg_iter.next().unwrap());
-    let (query_model_arg_name, query_model_type) = split_argument(arg_iter.next().unwrap());
+    let (event_arg_name, event_type) = split_argument(arg_iter.next().unwrap(), &ident_string, "event");
+    let (query_model_arg_name, query_model_type) = split_argument(arg_iter.next().unwrap(), &ident_string, "query model");
 
-    let event_type_ident = get_type_ident(event_type);
+    let event_type_ident = get_type_ident(event_type, &ident_string, "event");
     // println!("Event type ident: {:?}", event_type_ident);
     let event_type_literal = LitStr::new(&event_type_ident.to_string(), event_type_ident.span());
 
@@ -67,19 +67,114 @@ fn impl_event_handler(ast: &ItemFn) -> TokenStream {
     gen.into()
 }
 
-fn split_argument(argument: &FnArg) -> (&Ident, &Box<Type>) {
+#[proc_macro_attribute]
+pub fn command_handler(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    // Construct a representation of Rust code as a syntax tree
+    // that we can manipulate
+    let input = parse_macro_input!(item as ItemFn);
+
+    // Build the trait implementation
+    impl_command_handler(&input)
+}
+
+fn impl_command_handler(ast: &ItemFn) -> TokenStream {
+    // println!("AST: {:?}", ast);
+    let ItemFn{sig, block, ..} = ast;
+    let Signature {ident, inputs, output, ..} = sig;
+
+    let ident_string = ident.to_string();
+    let ident_span = ident.span();
+    // println!("X: {:?}: {:?}: {:?}", ident, ident_string, ident_span);
+    let ident_impl = Ident::new(&format!("{}_impl", ident_string), ident_span);
+
+    let mut arg_iter = inputs.iter();
+    let (command_arg_name, command_type) = split_argument(arg_iter.next().unwrap(), &ident_string, "command");
+    let (context_arg_name, context_type) = split_argument(arg_iter.next().unwrap(), &ident_string, "context");
+    let context_elem_type = get_elem_type_argument(context_type, &ident_string, "context");
+
+    let command_type_ident = get_type_ident(command_type, &ident_string, "command");
+    // println!("Event type ident: {:?}", event_type_ident);
+    let command_type_literal = LitStr::new(&command_type_ident.to_string(), command_type_ident.span());
+
+    let (output_type, output_type_ident) = match output {
+        ReturnType::Type(_, t) => (t, get_return_type_ident(&**t, &ident_string, "result")),
+        _ => panic!("Missing output type: {:?}", ident)
+    };
+    let output_type_literal = LitStr::new(&output_type_ident.to_string(), output_type_ident.span());
+
+    let gen = quote! {
+        use dendrite::axon_utils;
+        use dendrite::axon_utils::HandlerRegistry;
+
+        // register command handler with registry
+        fn #ident(registry: &mut axon_utils::TheHandlerRegistry<std::sync::Arc<async_lock::Mutex<#context_elem_type>>,axon_utils::SerializedObject>) -> Result<()> {
+            registry.insert_with_output(
+                #command_type_literal,
+                &#command_type::decode,
+                &(|c,p| Box::pin(#ident_impl(c, p)))
+            )
+        }
+
+        async fn #ident_impl(#command_arg_name: #command_type, #context_arg_name: std::sync::Arc<async_lock::Mutex<#context_elem_type>>) -> Result<Option<SerializedObject>> {
+            let mut #context_arg_name = #context_arg_name.deref().lock().await;
+            debug!("Event type: {:?}", #command_type_literal);
+            let result : #output_type = #block;
+            let result: Option<Result<SerializedObject>> = result?.map(|r| axon_utils::axon_serialize(#output_type_literal, &r));
+            match result {
+                Some(Ok(serialized)) => Ok(Some(serialized)),
+                Some(Err(e)) => Err(e),
+                None => Ok(None),
+            }
+        }
+    };
+    gen.into()
+}
+
+fn split_argument<'a>(argument: &'a FnArg, handler_name: &str, qualifier: &str) -> (&'a Ident, &'a Box<Type>) {
     if let FnArg::Typed(PatType {pat, ty, ..}) = argument {
         if let Pat::Ident(PatIdent {ref ident, ..}) = **pat {
             return (ident, ty);
         }
     }
-    panic!()
+    panic!("Can't parse argument: {:?}: {:?}", handler_name, qualifier)
 }
 
-fn get_type_ident(ty: &Type) -> &Ident {
+fn get_elem_type_argument<'a>(argument: &'a Type, handler_name: &str, qualifier: &str) -> &'a Box<Type> {
+    println!("Get elem type of: {:?}", argument);
+    if let Type::Reference(TypeReference { elem, .. }) = argument {
+        return elem;
+    }
+    panic!("Can't get element type of reference: {:?}: {:?}", handler_name, qualifier)
+}
+
+fn get_type_ident<'a>(ty: &'a Type, handler_name: &str, qualifier: &str) -> &'a Ident {
     if let Type::Path(TypePath {path: Path {segments, ..},..}) = ty {
         let last_segment = segments.last().unwrap();
         return &last_segment.ident;
     }
-    panic!()
+    panic!("Can't get type identifier: {:?}: {:?}", handler_name, qualifier)
+}
+
+fn get_return_type_ident<'a>(ty: &'a Type, handler_name: &str, qualifier: &str) -> &'a Ident {
+    let ty = get_first_generic_type_argument(ty, handler_name, qualifier);
+    let ty = get_first_generic_type_argument(ty, handler_name, qualifier);
+    if let Type::Path(TypePath {path:Path {segments:arg_segments,..}, ..}) = ty {
+        let last_arg_segment = arg_segments.last().unwrap();
+        let PathSegment { ident, ..} = last_arg_segment;
+        return ident;
+    }
+    panic!("Can't get return type identifier: {:?}: {:?}", handler_name, qualifier)
+}
+
+fn get_first_generic_type_argument<'a>(ty: &'a Type, handler_name: &str, qualifier: &str) -> &'a Type {
+    println!("Try to get first generic type argument: {:?}", ty);
+    if let Type::Path(TypePath { path: Path { segments, .. }, .. }) = ty {
+        let last_segment = segments.last().unwrap();
+        if let PathSegment { arguments: PathArguments::AngleBracketed(AngleBracketedGenericArguments { args, .. }), .. } = last_segment {
+            if let Some(GenericArgument::Type(result)) = args.first() {
+                return result;
+            }
+        }
+    }
+    panic!("Can't get first generic type argument: {:?}: {:?}", handler_name, qualifier)
 }
